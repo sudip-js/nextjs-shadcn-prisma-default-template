61b7fa0c9054d4f48e2d8e1a04cc2a01444d6329
sudip-js <sudipbanerjee305@gmail.com>
2026-02-04 23:08:34 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
+import db from "@/lib/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const user = await db.user.findMany();
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -13,6 +16,15 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <Button>Shadcn Button</Button>
+          <code>
+            {user.map((user) => (
+              <div key={user.id}>
+                <p>{user.name}</p>
+                <p>{user.email}</p>
+              </div>
+            ))}
+          </code>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
